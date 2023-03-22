cb68154f880b01c63c7ea0131ab9e39986d2ad87
Casey Corcoran <casecorc@amazon.com>
2023-03-22 12:25:54 -0400

    wip

--- src/components/app-root.tsx
+++ src/components/app-root.tsx
@@ -1,25 +1,38 @@
-import { Component, h, Host, State } from '@stencil/core/internal';
+import { Component, h, Host, State, Watch } from '@stencil/core/internal';
 
 import { someUtilityFunction } from 'src/utils';
-import {} from '@nanopop/';
+import { createPopper, NanoPop } from '@nanopop';
 
 @Component({
   tag: 'app-root',
 })
 export class Root {
   @State() showPopper = false;
+  @Watch('showPopper')
+  onPopper(value) {
+    if (value) {
+      this.popper.update()
+    }
+  }
+  buttonEl: HTMLButtonElement;
+  popperEl: HTMLElement;
+  popper: NanoPop;
   componentDidLoad() {
     someUtilityFunction();
+    this.popper = createPopper(this.buttonEl, this.popperEl, {
+      position: 'bottom'
+    })
   }
   render() {
     return (
-      <Host class="p-12 flex">
-        <button onClick={() => (this.showPopper = !this.showPopper)} class="py-1 px-2 rounded-sm bg-blue-100">
+      <Host class="flex w-screen h-screen justify-center items-center">
+        <button ref={el => this.buttonEl = el} onClick={() => (this.showPopper = !this.showPopper)} class="py-1 px-2 rounded-sm bg-blue-100">
           Click me
         </button>
         <aside
+          ref={el => this.popperEl = el}
           class={{
-            'py-1 px-2 rounded-sm shadow-lg border text-blue-500 border-blue-500': true,
+            'flex fixed py-1 px-2 rounded-sm shadow-lg border text-blue-500 border-blue-500': true,
             'invisible': !this.showPopper,
           }}
         >
